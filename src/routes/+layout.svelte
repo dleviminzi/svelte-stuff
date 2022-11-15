56be89150710b637331a6e5a178a9debe17a737f
Daniel Levi-Minzi <dleviminzi@gmail.com>
2022-11-15 10:27:00 -0500

    initial stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+	import '../app.css';
+	const inactiveLinkClass =
+		'text-left text-limed-spruce-500 hover:text-regent-st-blue-500 text-xl col-span-1 md:col-span-6 justify-start';
+	const activeLinkClass =
+		'text-left text-regent-st-blue-500 text-xl col-span-1 md:col-span-6 justify-start';
+
+	let current = 'index';
+</script>
+
+<nav class="not-prose md:grid md:grid-cols-6">
+	<a
+		href="/"
+		class={current === 'index' ? activeLinkClass : inactiveLinkClass}
+		on:click={() => (current = 'index')}>Home</a
+	>
+	<a
+		href="/about"
+		class={current === 'about' ? activeLinkClass : inactiveLinkClass}
+		on:click={() => (current = 'about')}>About</a
+	>
+	<a
+		href="/settings"
+		class={current === 'settings' ? activeLinkClass : inactiveLinkClass}
+		on:click={() => (current = 'settings')}>Settings</a
+	>
+</nav>
+
+<slot />
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	let lessons = [
+		{ lessonName: 'class 1', lessonDescription: 'do first stuff' },
+		{ lessonName: 'class 2', lessonDescription: 'then do more stuff' },
+		{ lessonName: 'class 3', lessonDescription: 'move on to the new stuff' }
+	];
+
+	function add() {
+		lessons = lessons.concat({ lessonName: '', lessonDescription: '' });
+	}
+</script>
+
+<h1>Lessons</h1>
+
+<div class="flex overflow-x-auto space-x-8 w-3/4">
+	{#each lessons as lesson}
+		<div>
+			<input placeholder="give this lesson a title" bind:value={lesson.lessonName} />
+			<input placeholder="give this lesson a description" bind:value={lesson.lessonDescription} />
+		</div>
+	{/each}
+
+	<button on:click={add}> Add new lesson to week </button>
+</div>
